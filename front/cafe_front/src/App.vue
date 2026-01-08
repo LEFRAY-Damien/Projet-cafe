9e9c01e9f988592e8092e839d28a13c8f28d4ef4
damienlefray <damienlefray@gmail.com>
2026-01-08 10:22:06 -0500

    acceuil.vue ok

--- front/cafe_front/src/App.vue
+++ front/cafe_front/src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <RouterLink to="/">Accueil</RouterLink> |
-    <RouterLink to="/produits">Produits</RouterLink> |
-    <RouterLink to="/login">Se connecter</RouterLink> |
-    <RouterLink to="/contact">Contact</RouterLink> |
-    <RouterLink to="/info">Info</RouterLink>
+  <nav class="navbar navbar-expand-lg navbar-dark text-white bg-transparent position-absolute top-0 w-100"
+    style="z-index:10;">
+
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Café Français</RouterLink>
+
+      <div class="navbar-nav">
+        <RouterLink class="nav-link" to="/info">Info</RouterLink>
+        <RouterLink class="nav-link" to="/produits">Produits</RouterLink>
+        <RouterLink class="nav-link" to="/contact">Contact</RouterLink>
+        <RouterLink class="nav-link" to="/login">Se connecter</RouterLink>
+      </div>
+    </div>
   </nav>
 
   <router-view />
--- front/cafe_front/src/views/HomeView.vue
+++ front/cafe_front/src/views/HomeView.vue
@@ -1,4 +1,10 @@
 <template>
-  <h1>Accueil</h1>
-  <p>Bienvenue sur Café Français.</p>
+  <div class="home-bg d-flex align-items-center justify-content-center">
+    <div class="home-overlay"></div>
+
+    <div class="home-content text-center text-white">
+      <h1>Café Français</h1>
+      <p>Le goût du vrai café</p>
+    </div>
+  </div>
 </template>
