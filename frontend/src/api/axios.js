731e1fbabf80be5471a1af613ff4506b9023e2d6
damienlefray <damienlefray@gmail.com>
2026-01-25 14:41:20 +0100

    switch environement docker

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8080",
+  /* baseURL: "http://localhost:8000",*/
   headers: {
     Accept: "application/ld+json",
     "Content-Type": "application/ld+json",
