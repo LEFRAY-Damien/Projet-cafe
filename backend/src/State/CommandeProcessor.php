812eb9af933ac77d898daeacf96a21ca506328ca
damienlefray <damienlefray@gmail.com>
2026-02-07 12:11:42 +0100

    panier sans date ok

--- backend/src/State/CommandeProcessor.php
+++ backend/src/State/CommandeProcessor.php
@@ -6,11 +6,15 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use App\Entity\Commande;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class CommandeProcessor implements ProcessorInterface
 {
     public function __construct(
         private Security $security,
+
+        // ✅ on remet le NOM attendu : $writeProcessor
+        #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
         private ProcessorInterface $writeProcessor,
     ) {}
 
