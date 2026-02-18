18a2078a90f700834f5ed5df953fe1fb1293e4a5
damienlefray <damienlefray@gmail.com>
2026-02-18 11:21:40 +0100

    Fix Route import for prod

--- backend/src/Controller/MeFavorisController.php
+++ backend/src/Controller/MeFavorisController.php
@@ -7,7 +7,7 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
